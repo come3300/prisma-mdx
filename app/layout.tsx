6699ecc6959bb3aaab6351398aec32de3c0ffa66
Daisuke Yonekura <come.vanho330@icloud.com>
2024-01-10 22:44:08 +0900

    MDX　パッケージ　全追加　設定完了！

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,5 +50,19 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
 // 新しい Layout コンポーネント
 export function Layout({ children }: { children: React.ReactNode }) {
-  return <article className="prose prose-xl">{children}</article>;
+  return (
+    <>
+      <link
+       rel="stylesheet"
+       href="https://cdn.jsdelivr.net/npm/katex@0.15.3/dist/katex.min.css"
+       integrity="sha384-KiWOvVjnN8qwAZbuQyWDIbfCLFhLXNETzBQjA/92pIowpC0d2O3nppDGQVgwd2nB"
+       crossOrigin="anonymous"
+     />
+       <link
+         href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.9.0/themes/prism-tomorrow.min.css"
+         rel="stylesheet"
+       />
+      <article className="prose prose-xl">{children}</article>
+    </>
+  );
 }
